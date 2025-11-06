aeff7b589e04d07ce32a4e6b7efc8a102cfb44a0
Vitor Alcantara <vitoralcan5@gmail.com>
2025-11-06 18:31:09 -0300

    feat: dropshadow initial paragraph

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,25 @@ export default function Header() {
         <div>
             <nav className='flex justify-around list-none items-center h-50 text-xl text-gray-400'>
                 <div className='flex w-60 justify-between'>
-                    <li className='duration-300 hover:scale-105 hover:text-gray-50 cursor-pointer'>PORTFOLIO</li>
-                    <li className='duration-300 hover:scale-105 hover:text-gray-50 cursor-pointer'>RESUMO</li>
+                    <li className='duration-300 hover:scale-105 hover:text-gray-50 cursor-pointer hover:font-semibold'>
+                        PORTFOLIO
+                    </li>
+                    <li className='duration-300 hover:scale-105 hover:text-gray-50 cursor-pointer hover:font-semibold'>
+                        RESUMO
+                    </li>
                 </div>
+                
                 <li className='duration-300 hover:scale-110 hover:cursor-pointer'>
-                    <img className='w-20 hover:drop-shadow-[0_0_60px_#00FFAA]' src={logo} alt="Logotipo" /></li>
+                    <img className='w-20 hover:drop-shadow-[0_0_60px_#00FFAA]' src={logo} alt="Logotipo" />
+                </li>
+                
                 <div className='flex w-60 justify-between'>
-                    <li className='duration-300 hover:scale-105 hover:text-gray-50 cursor-pointer'>SOBRE MIM</li>
-                    <li className='duration-300 hover:scale-105 hover:text-gray-50 cursor-pointer'>CONTATO</li>
+                    <li className='duration-300 hover:scale-105 hover:text-gray-50 cursor-pointer hover:font-semibold'>
+                        SOBRE MIM
+                    </li>
+                    <li className='duration-300 hover:scale-105 hover:text-gray-50 cursor-pointer hover:font-semibold'>
+                        CONTATO
+                    </li>
                 </div>
             </nav>
         </div>
--- src/page/Portfolio.jsx
+++ src/page/Portfolio.jsx
@@ -8,7 +8,7 @@ export default function Portfolio() {
             <h1 className="text-3xl uppercase font- font-[Raleway] font-semibold">
                 Olá! sou Vitor <span className="text-[#00FFAA]">Alcantara</span></h1>
             
-            <p className="text-6xl mt-5 text-[] font-bold uppercase">Desenvolvedor full-stack, apaixonado por tecnologia e por criar soluções que facilitam a vida das pessoas.</p>
+            <p className="text-6xl mt-5 text-[] font-bold uppercase drop-shadow-[0_20px_110px_#00FFFA]">Desenvolvedor full-stack, apaixonado por tecnologia e por criar soluções que facilitam a vida das pessoas.</p>
         </div>
     </div>
   )
